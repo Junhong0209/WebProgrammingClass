624a15350c0e8cc5cf0ea9d70f56847a35169da0
Junhong0209 <junh040209@naver.com>
2021-05-27 14:10:33 +0900

    [ADD] - Vue.js
    Vue.js 수업 자료

---
+++ vuetest/src/components/FriendCrad.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="friend">
+        <div><img v-bind:src="friend.image"></div>
+        <div class="friend-content">
+            <h3 class="friend-name">{{ friend.name }}</h3>
+            <span>{{ friend.content }}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FriendCard',
+    props: ['friend']
+}
+</script>
+
+<style lang="scss" scoped>
+.friend {
+    border: 1px solid #aaa;
+    width: 200px;
+    display: inline-block;
+    vertical-align: top;
+    margin: 8px;
+
+    img {
+      width: 100%;
+    }
+}
+.friend-content {
+    padding: 10px;
+    border-top: 1px solid #aaa;
+
+    h3 {
+      text-align: center;
+      margin: 8px 0;
+    }
+}
+</style>
